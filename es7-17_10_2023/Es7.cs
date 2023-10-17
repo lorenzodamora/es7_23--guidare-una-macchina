4ca2d01fb0e56a6dafc0b9a0a25c550507319b5b
aula E2-9 pc18 <damora.lorenzo.studente@itispaleocapa.it>
2023-10-17 12:30:08 +0200

    Aggiungere i file di progetto.

---
+++ es7-17_10_2023/Es7.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace es7_17_10_23
+{
+	public partial class Es7 : Form
+	{
+		public Es7()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ es7-17_10_2023/Macchina.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace es7_17_10_23
+{
+	internal class Macchina
+	{
+
+	}
+}
