33a02f231318a36030216656cd21130b81254abd
aula E2-9 pc18 <damora.lorenzo.studente@itispaleocapa.it>
2023-10-17 12:36:52 +0200

    trascritto l'esercizio

--- es7-17_10_2023/Es7.cs
+++ es7-17_10_2023/Es7.cs
@@ -5,6 +5,15 @@ namespace es7_17_10_23
 {
 	public partial class Es7 : Form
 	{
+		/*
+		 * Realizzare un programma in windows form che consenta di simulare la guida di un'automobile.
+		 * Le funzionalità richieste sono:
+		 * accende e spegnere il motore;
+		 * accelerare e decelerare;
+		 * aumentare e diminuire la marcia..
+		 * Per la realizzazione creare la classe Auto con gli attributi e i metodi opportuni.
+		 * Creare un repository GitHub effettuando almeno un commit/push,  per ogni funzionalità implementata
+		 */
 		public Es7()
 		{
 			InitializeComponent();
