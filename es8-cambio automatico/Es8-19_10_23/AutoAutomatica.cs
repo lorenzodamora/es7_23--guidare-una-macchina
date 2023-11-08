c54c9ef3c65f7ac3ea0e77361b998c6496cb2500
lorenzodamora <damora.lorenzo.studente@itispaleocapa.it>
2023-11-08 20:37:04 +0100

    funzione calcoloMarcia

--- es8-cambio automatico/Es8-19_10_23/AutoAutomatica.cs
+++ es8-cambio automatico/Es8-19_10_23/AutoAutomatica.cs
@@ -1,5 +1,5 @@
 ﻿using System; //Math, Exception, string
-/*using System.Linq; //Dictionary .First() e .Last() */
+using System.Linq; //Dictionary .First() e .Last()
 
 namespace es8_19_10_23
 {
@@ -9,6 +9,10 @@ namespace es8_19_10_23
 	/// <remarks>è senza cambio manuale.</remarks>
 	internal class AutoAutomatica : Auto
 	{
+		public bool Mode //0 retro, 1 avanti
+		{
+			get; set;
+		} = true;
 
 		private bool _mode; //0 avanti, 1 retro
 
@@ -19,6 +23,20 @@ namespace es8_19_10_23
 		}
 
 		public override string CambiaMarcia(Gears gear) => "Now;Cambio Marcia;;in questa auto la marcia non può essere cambiata manualmente";
+
+		private short CalcoloMarcia()
+		{
+			if(!Mode) return -1;
+			for(short gear = 1; gear < speedAndAccPerGear.Keys.Count-1; gear++)
+				if(Speed <= speedAndAccPerGear[gear].maxSpeed && Speed >= speedAndAccPerGear[gear].minSpeed) return gear;
+			if(Speed < 0) return 1;
+			short first = speedAndAccPerGear.Keys.First();
+			if(Speed < speedAndAccPerGear[first].minSpeed) return first;
+			short last = speedAndAccPerGear.Keys.Last();
+			if(Speed > speedAndAccPerGear[last].maxSpeed) return last;
+
+			throw new Exception("non è stata trovata la marcia corretta");
+		}
 
 
 	}
