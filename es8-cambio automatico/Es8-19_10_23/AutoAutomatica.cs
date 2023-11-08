7241eb340979f44bd1f83655307f3de6d088bff0
lorenzodamora <damora.lorenzo.studente@itispaleocapa.it>
2023-11-08 20:30:26 +0100

    aggiunta classe derivata e downgrade versione

---
+++ es8-cambio automatico/Es8-19_10_23/AutoAutomatica.cs
@@ -0,0 +1,23 @@
+﻿using System; //Math, Exception, string
+/*using System.Linq; //Dictionary .First() e .Last() */
+
+namespace es8_19_10_23
+{
+	/// <summary>
+	/// Macchina con cambio automatico.
+	/// </summary>
+	/// <remarks>è senza cambio manuale.</remarks>
+	internal class AutoAutomatica : Auto
+	{
+
+		private bool _mode; //0 avanti, 1 retro
+
+		public AutoAutomatica() : base()
+		{
+			//GearMax = 7;
+			//speedAndAccPerGear.Add(7,())
+		}
+
+
+	}
+}
