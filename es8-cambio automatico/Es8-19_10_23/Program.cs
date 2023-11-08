54ab8f8c6b7d2d388a53630bf1a85a9bc3e2c7e7
lorenzodamora <damora.lorenzo.studente@itispaleocapa.it>
2023-11-08 18:24:07 +0100

    aggiunta soluzione es8

---
+++ es8-cambio automatico/Es8-19_10_23/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Es8_19_10_23
+{
+	internal static class Program
+	{
+		/// <summary>
+		/// Punto di ingresso principale dell'applicazione.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+		}
+	}
+}
